a9a5d370ce49088a7be5a293d0474454a7742fae
kaawsdev <kagalpan@kacpp.xyz>
2021-02-01 20:35:03 +0900

    githubリポジトリにkibpykaライブラリを追加。

--- libpyka/__init__.py
+++ libpyka/__init__.py
@@ -1,8 +1,8 @@
 # libpykaライブラリ
 __copyright__    = 'Copyright (C) 2020 Kentarou Arakawa'
-__version__      = '0.1.5'
+__version__      = '0.1.6'
 __license__      = 'MIT'
 __author__       = 'Kentarou Arakawa'
 __author_email__ = 'kagalpan@kacpp.xyz'
-__url__          = 'https://www.kacpp.xyz/'
+__url__          = 'https://github.com/karakawa88/libpyka/'
 
